40c65d0e8be7a7bde2599c50df191df1ab74e9ca
petter <petter@sannsyn.com>
2016-08-09 23:21:10 +0200

    some tests for permutations

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -30,7 +30,6 @@ defmodule BL.Helpers do
   ## Examples
 
       iex> BL.Helpers.permuteAnswers([1,2],[3,4])
-
       [{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}]
 
   """
--- test/better_learning_test.exs
+++ test/better_learning_test.exs
@@ -1,8 +1,21 @@
 defmodule BLTest do
   use ExUnit.Case
-  doctest BL
+  doctest BL.Helpers
 
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "permutaions for only new exercises" do
+    assert  BL.Helpers.permuteAnswers([1,2,3], []) == [{1, 2}, {1, 3}, {2, 3}] 
+  end
+
+  test "permutaions for old and new exercises" do
+    assert  BL.Helpers.permuteAnswers([1,2], [3,4]) == [{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}]
+  end
+
+  test "permutations with no new ones" do
+    assert  BL.Helpers.permuteAnswers([], [3,4]) == []
+  end
+
 end
